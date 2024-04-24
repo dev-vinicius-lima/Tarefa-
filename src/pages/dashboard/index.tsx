39f60263425d151f4efe9c83f4f38a6093a04c1c
Vinicius Lima <viniciuslimaes@gmail.com>
2024-04-23 20:33:32 -0400

    feat: :sparkles: add dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,10 @@
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps } from "next";
 import styles from "./Dashboard.module.css";
 import Head from "next/head";
-import { getSession } from 'next-auth/react';
+import { getSession } from "next-auth/react";
+import Textarea from '@/components/Textarea';
+import { FiShare2 } from 'react-icons/fi';
+import { FaTrash } from 'react-icons/fa';
 
 export default function Dashboard() {
   return (
@@ -9,24 +12,57 @@ export default function Dashboard() {
       <Head>
         <title>Meu painel de tarefas</title>
       </Head>
-      <h1>Meu painel de tarefas</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+            <form >
+              <Textarea placeholder='Digite qual sua tarefa...' />
+              <div className={styles.checkBoxArea}>
+                <label>
+                  <input type="checkbox" className={styles.checkBox} />
+                  <span className={styles.checkBoxText}>Deixar tarefa pública?</span>
+                </label>
+              </div>
+              <button type='submit' className={styles.button}>Registrar</button>
+            </form>
+          </div>
+        </section>
+        <section className={styles.taskContainer}>
+          <h1 className={styles.title}>Minhas Tarefas</h1>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>Minha primeria tarefa de exemplo!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color='#ea3140' />
+              </button>
+            </div>
+          </article>
+        </section>
+      </main>
     </div>
   );
-};
+}
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const session = await getSession({ req })
-  console.log(session)
+  const session = await getSession({ req });
+  console.log(session);
   if (!session?.user) {
     // redirecionando usuario que nao esta logado
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
       },
-    }
+    };
   }
   return {
-    props: {}
-  }
-} 
+    props: {},
+  };
+};
