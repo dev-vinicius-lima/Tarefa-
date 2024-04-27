91b5f880996d4eab0c400040c34f1245614183f2
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-04-26 20:22:00 -0400

    test authentication google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth/next";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
